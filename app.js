a0fd6f2605da28be94e8b5d0c1ee16b315fb73c8
shubham rana <shubhamrana300999@gmail.com>
2021-07-05 23:13:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var path=require('path');
+var app = express()
+
+app.use(express.static('public'));
+var server = app.listen(3000,function(){
+    console.log("listening to port 3000")
+})
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'mainpage.html'))
+})
+
+app.get('/sudoku',function(req,res){
+    res.redirect('sudoku.html')
+})
